c156e097e2db1cd61aadf13b23b0de2f91fb045c
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-23 15:24:18 +0300

    feat: add Person entity

---
+++ WMS/WMS.Domain/Entities/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+  
+        public class Person
+        {
+            public int PersonID { get; private set; }
+            public string NationalID { get; private set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+
+            // Foreign Key and Navigation Property
+            public int CountryID { get; set; }
+            public Country CountryInfo { get; set; }
+
+            public Person() { }
+        }
+    
+}
