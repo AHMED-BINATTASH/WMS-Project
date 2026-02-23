e6417906ce38b0014e799434cccc4d3bfee6a3c0
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-23 15:48:56 +0300

    feat: Edit Person entity

--- WMS/WMS.Domain/Entities/Person.cs
+++ WMS/WMS.Domain/Entities/Person.cs
@@ -6,22 +6,29 @@ using System.Threading.Tasks;
 
 namespace WMS.Domain.Entities
 {
-  
-        public class Person
+
+    public class Person
+    {
+        public int PersonID { get; private set; }
+        public string NationalID { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // Foreign Key and Navigation Property
+        public int CountryID { get; set; }
+        public Country CountryInfo { get; set; }
+
+        public Person(Country country, int personID, string nationalID)
         {
-            public int PersonID { get; private set; }
-            public string NationalID { get; private set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Address { get; set; }
-            public string Phone { get; set; }
-            public string Email { get; set; }
+            CountryInfo = country;
+            PersonID = personID;
+            NationalID = nationalID;
 
-            // Foreign Key and Navigation Property
-            public int CountryID { get; set; }
-            public Country CountryInfo { get; set; }
 
-            public Person() { }
         }
-    
+    }
+
 }
