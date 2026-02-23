d6fea773069d9a871f2f87691544200443d0b153
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-23 15:19:05 +0300

    feat: add Country entity

---
+++ WMS/WMS.Domain/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class Country
+    {
+        public int CountryID { get; private set; } // Matches diagram: int private set
+        public string CountryName { get; set; }
+
+        // Constructor as shown in diagram
+        public Country(int countryID)
+        {
+            CountryID = countryID;
+        }
+
+      
+    }
+}
