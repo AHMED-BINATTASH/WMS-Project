24584dc164a166b0d963e779f740078f441dc9f2
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-23 15:30:02 +0300

    feat: add User entity

---
+++ WMS/WMS.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class User
+    {
+        public int UserID { get; private set; }
+        public int PersonID { get; private set; }
+
+     
+        public Person PersonInfo { get; set; }
+
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+       
+        public User(Person person, int personID ,int userID)
+        {
+            PersonInfo = person;
+            PersonID = personID;
+            UserID = userID;
+        }
+
+      
+    }
+}
