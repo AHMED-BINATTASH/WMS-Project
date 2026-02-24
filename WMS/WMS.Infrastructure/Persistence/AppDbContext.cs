6f00e8047de51e98defe474a671465e41d040100
AHMED-BINATTASH <abinattash@gmail.com>
2026-02-24 16:03:26 +0300

    add: Infrastructure Configurations

--- WMS/WMS.Infrastructure/Persistence/AppDbContext.cs
+++ WMS/WMS.Infrastructure/Persistence/AppDbContext.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.Domain.Entities;
+using WMS.Infrastructure.Configurations;
 
 namespace WMS.Infrastructure.Persistence
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CountryConfiguration).Assembly);
+        }
     }
 }
--- WMS/WMS.Presentation/Program.cs
+++ WMS/WMS.Presentation/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WMS.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registring AppDbContext Option
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(builder.Configuration["ConnectionString"],
+    b => b.MigrationsAssembly("WMS.Infrastructure")));
 
 var app = builder.Build();
 
