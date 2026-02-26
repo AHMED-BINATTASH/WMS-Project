e9448fbca7698a8e1e3d5e408c92126125487679
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-26 23:29:23 +0300

    feat: Edit ICountryService

--- WMS/WMS.Application/Interfaces/ICountryService.cs
+++ WMS/WMS.Application/Interfaces/ICountryService.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.Application.DTOs;
 
 namespace WMS.Application.Interfaces
 {
-    internal interface ICountryService
+
+    public interface ICountryService
     {
+        // 
+        Task<IEnumerable<CountryDto>> GetAvailableCountries();
     }
+
 }
