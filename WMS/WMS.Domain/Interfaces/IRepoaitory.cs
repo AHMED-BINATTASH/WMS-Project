b5da09f58212546d189127f6be17883c7e66507d
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-27 15:42:16 +0300

    feat: Add IRepositry to Dmain Layer

---
+++ WMS/WMS.Domain/Interfaces/IRepoaitory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Domain.Entities;
+
+namespace WMS.Domain.Interfaces
+{
+    internal interface IRepoaitory<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task Add(T entity);
+        Task Delete(int id);
+        Task Update();
+    }
+}
