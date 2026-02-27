ba0d329809535b2688c332eca98ab8661a79402d
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-02-27 17:53:42 +0300

    feat: Add User Dto

---
+++ WMS/WMS.Application/DTOs/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Domain.Entities;
+
+namespace WMS.Application.DTOs
+{
+    public record UserDto(int UserID,
+     int PersonID,
+     Person Person,
+     string Username,
+     string Role,
+     string Password,
+     bool IsActive);
+}
