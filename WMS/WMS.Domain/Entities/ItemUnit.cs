9564fcee114628f32c2a23877148daeb3ef6bd70
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-03 00:53:04 +0300

    feat: add ItemUnit entity

---
+++ WMS/WMS.Domain/Entities/ItemUnit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class ItemUnit
+    {
+        public int ItemUnitID { get; private set; }
+        public int ItemID { get; set; }
+        public int UnitID { get; set; }
+        public short? Factor { get; set; }
+
+        // This constructor for EF Core to can create instance from ItemUnit class
+        private ItemUnit() { }
+
+        // Constructor 
+        public ItemUnit(int itemUnitId)
+        {
+            this.ItemUnitID = itemUnitId;
+        }
+    }
+}
