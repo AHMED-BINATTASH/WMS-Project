9ec2f02b2defcaecd4c969b2b696765fcb39dfb1
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-03 00:54:06 +0300

    feat: add Item entity

---
+++ WMS/WMS.Domain/Entities/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class Item
+    {
+        // Properties
+        public int ItemID { get; private set; }
+        public string ItemName { get; set; }
+        public string Barcode { get; set; }
+        public int UnitID { get; private set; }
+        public Unit UnitInfo { get; set; }
+        public int CategoryID { get; private set; }
+        public Category CategoryInfo { get; set; }
+        public decimal ReorderPoint { get; set; }
+        public decimal AverageCost { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsExpiryRelated { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+
+        // This constructor for EF Core to can create instance from Item class
+        private Item() { }
+
+        // Constructor
+        public Item(Category category, Unit unit, int itemID)
+        {
+            this.CategoryInfo = category;
+            this.CategoryID = category.CategoryID;
+            this.UnitInfo = unit;
+            this.UnitID = unit.UnitID;
+            this.ItemID = itemID;
+            this.IsActive = true;
+            this.CreatedAt = DateTime.Now;
+        }
+
+        // Methods
+        public void SoftDelete()
+        {
+            this.IsActive = false;
+        }
+    }
+}
