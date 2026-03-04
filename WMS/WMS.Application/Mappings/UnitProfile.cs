e10019d8c4f6973c6a23b63d86b5c05dd93380b4
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-05 01:02:03 +0300

    feat: Add Unit Profile

---
+++ WMS/WMS.Application/Mappings/UnitProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Application.DTOs;
+using WMS.Domain.Entities;
+
+
+namespace WMS.Application.Mappings
+{
+    public class UnitProfile :Profile
+    {
+        public UnitProfile()
+        {
+            CreateMap<Unit,UnitDto>();
+            CreateMap<UnitDto,Unit>();
+        }
+    }
+}
