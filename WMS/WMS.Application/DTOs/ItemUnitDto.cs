57f58d18eaf93b4a76b62af58cc1baf19fff4304
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-05 01:31:06 +0300

    feat: Add ItemUnit Dto

---
+++ WMS/WMS.Application/DTOs/ItemUnitDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Application.DTOs
+{
+    public record ItemUnitDto(
+        int ItemUnitID,
+        int ItemID,
+        int UnitID,
+        short? Factor
+    );
+}
