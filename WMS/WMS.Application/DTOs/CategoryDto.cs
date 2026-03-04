14e817b793a4add4564e9de152720971aa0632c2
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-05 00:10:43 +0300

    feat: Add CategroyDto

---
+++ WMS/WMS.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public record CategoryDto(
+        int CategoryId,
+        string CategoryName,
+        string? Description,
+        int? ParentCategory
+    );
+}
+
+
+
