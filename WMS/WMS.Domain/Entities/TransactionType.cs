cea71a9fc6fec9ed24d8d4c5c20eb499068c7823
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-06 01:04:43 +0300

    feat: Add TransactionType entity

---
+++ WMS/WMS.Domain/Entities/TransactionType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class TransactionType
+    {
+        public int TransactionTypeID { get; private set; }
+        public string TransactionTypeName { get; set; }
+        public string Description { get; set; }
+
+        // This constructor for EF Core to can create instance from TransactionType class
+        private TransactionType() { }
+
+        public TransactionType(int transactionTypeID)
+        {
+            this.TransactionTypeID = transactionTypeID;
+        }
+    }
+}
