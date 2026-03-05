ab36a7ac4cf8509dd579c8193ac83419067d3037
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-06 00:51:14 +0300

    feat: Add Warehouse entity

---
+++ WMS/WMS.Domain/Entities/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS.Domain.Entities
+{
+    public class Warehouse
+    {
+       
+        public int WarehouseID { get; private set; }
+        public string WarehouseCode { get; set; }
+        public string WarehouseName { get; set; }
+        public string Location { get; set; }
+        public bool IsActive { get; set; }
+
+        // This constructor for EF Core to can create instance from Item class
+        private Warehouse() { }
+       
+        public Warehouse(int warehouseID)
+        {
+            WarehouseID = warehouseID;
+        }
+      
+        public void SoftDelete()
+        {
+            // it will avarable soon
+        }
+    }
+}
