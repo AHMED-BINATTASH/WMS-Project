ca1281a642b414850f785f5b203e28a358e7f775
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-10 23:22:09 +0300

    feat: Add WarehouseStock Service

---
+++ WMS/WMS.Application/DTOs/WarehouseStockDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Domain.Entities;
+
+namespace WMS.Application.DTOs
+{
+    public record WarehouseStockDto(
+     int WarehouseStockID,
+     int WarehouseID,
+     Warehouse WarehouseInfo, 
+     int ItemID,
+     Item ItemInfo,      
+     int Quantity,
+     string BatchNumber,
+     decimal ActualCost,
+     DateTime ProductionDate,
+     DateTime ExpiryDate,
+     DateTime CreatedAt,
+     int CreatedBy,
+     User CreatorInfo
+ );
+}
---
+++ WMS/WMS.Application/Services/WarehouseStockService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Application.DTOs;
+using WMS.Application.Interfaces;
+using WMS.Domain.Entities;
+using WMS.Domain.Interfaces;
+
+namespace WMS.Application.Services
+{
+    public class WarehouseStockService : IService<WarehouseStockDto, Warehouse>
+    {
+        private readonly IRepository<Warehouse> _repository;
+        private readonly IMapper _mapper;
+
+        public WarehouseStockService(IRepository<Warehouse> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<bool> AddNew(Warehouse Entity)
+        {
+            return await _repository.Add(Entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.Delete(id);
+        }
+
+        public async Task<IEnumerable<WarehouseStockDto>?> GetAll()
+        {
+            var warehouseStocks = await _repository.GetAllAsync();
+            return warehouseStocks != null ? _mapper.Map<IEnumerable<WarehouseStockDto>>(warehouseStocks) : null;
+        }
+
+        public async Task<WarehouseStockDto?> GetByID(int id)
+        {
+            var warehouseStock = await _repository.GetByIdAsync(id);
+            return warehouseStock != null ? _mapper.Map<WarehouseStockDto>(warehouseStock) : null;
+        }
+
+        public async Task<bool> Update(Warehouse Entity)
+        {
+            return await _repository.Update(Entity);
+        }
+    }
+}
