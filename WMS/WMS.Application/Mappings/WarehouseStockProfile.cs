21e47601310d904686fb2b55159cba1a45da29ca
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-10 23:42:02 +0300

    feat: add WarehouseStock Service v2

--- WMS/WMS.Application/Mappings/WarehouseStockProfile.cs
+++ WMS/WMS.Application/Mappings/WarehouseStockProfile.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.Application.DTOs;
+using WMS.Domain.Entities;
 
 namespace WMS.Application.Mappings
 {
-    internal class WarehouseStockProfile
+    public class WarehouseStockProfile :Profile
     {
+        public WarehouseStockProfile()
+        {
+            CreateMap<WarehouseStock, WarehouseStockDto>().ReverseMap();
+        }
     }
 }
