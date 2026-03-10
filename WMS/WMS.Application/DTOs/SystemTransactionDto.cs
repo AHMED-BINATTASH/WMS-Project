74a2fbdd41eef791d793e1f9bafd6c6ec392c487
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-11 00:25:40 +0300

    feat: Add System Transaction Service

---
+++ WMS/WMS.Application/DTOs/SystemTransactionDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Domain.Entities;
+
+namespace WMS.Application.DTOs
+{
+    public record SystemTransactionDto(
+     int TransactionID,
+     int WarehouseID,
+     Warehouse WarehouseInfo, 
+     int ItemID,
+     Item ItemInfo,
+     int TransactionTypeID,
+     TransactionType TransactionTypeInfo,
+     int Quantity,
+     decimal RunningBalance,
+     string Description,
+     string ReferenceNumber,
+     DateTime CreatedAt,
+     int CreatedBy,
+     User CreatorInfo
+ );
+}
--- WMS/WMS.Application/Services/TransactionService.cs
+++ WMS/WMS.Application/Services/TransactionService.cs
@@ -1,21 +1,58 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using WMS.Application.DTOs;
+using WMS.Application.Interfaces;
+using WMS.Domain.Entities;
 using WMS.Domain.Interfaces;
 
 namespace WMS.Application.Services
 {
-    public class TransactionService
+    public class TransactionService : IService<SystemTransactionDto, SystemTransaction>
     {
        
         private readonly ITransaction _transaction;
+        private readonly IRepository<SystemTransaction> _repository;
+        private readonly IMapper _mapper;
 
-      
-        public TransactionService(ITransaction transaction)
+        public TransactionService(IRepository<SystemTransaction> repository, IMapper mapper, ITransaction transaction)
         {
+            _mapper = mapper;
+            _repository = repository;
             _transaction = transaction;
+        }
+
+        public async Task<bool> AddNew(SystemTransaction Entity)
+        {
+            return await _repository.Add(Entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.Delete(id);
+        }
+
+        public async Task<IEnumerable<SystemTransactionDto>?> GetAll()
+        {
+           IEnumerable< SystemTransaction> transactions =await _repository.GetAllAsync();
+
+            return _mapper.Map<IEnumerable<SystemTransactionDto>>(transactions);
+
+        }
+
+        public async Task<SystemTransactionDto?> GetByID(int id)
+        {
+            SystemTransaction transaction  = await _repository.GetByIdAsync(id);
+            return _mapper.Map<SystemTransactionDto>(transaction);
+        }
+
+        public async Task<bool> Update(SystemTransaction Entity)
+        {
+            return await _repository.Update(Entity);
         }
 
         public void SaveTransaction()
