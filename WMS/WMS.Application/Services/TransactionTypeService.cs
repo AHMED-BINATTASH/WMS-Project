b46b6c32b39ed50fbccc66081e2b561cb7d86460
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-03-10 23:52:50 +0300

    feat: Add TransactionType Service

---
+++ WMS/WMS.Application/Services/TransactionTypeService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Application.DTOs;
+using WMS.Application.Interfaces;
+using WMS.Domain.Entities;
+using WMS.Domain.Interfaces;
+
+namespace WMS.Application.Services
+{
+    public class TransactionTypeService : IService<TransactionTypeDto, TransactionType>
+    {
+        private readonly IRepository<TransactionType> _repository;
+        private readonly IMapper _mapper;
+        public TransactionTypeService(IRepository<TransactionType> repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        public async Task<bool> AddNew(TransactionType Entity)
+        {
+            return await _repository.Add(Entity);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.Delete(id);
+        }
+
+        public async Task<IEnumerable<TransactionTypeDto>?> GetAll()
+        {
+            var TransactionTypes = await _repository.GetAllAsync();
+
+            return TransactionTypes != null ? _mapper.Map<IEnumerable<TransactionTypeDto>>(TransactionTypes) : null;
+
+        }
+
+        public async Task<TransactionTypeDto?> GetByID(int id)
+        {
+            var TransactionType = await _repository.GetByIdAsync(id);
+
+            return TransactionType != null ? _mapper.Map<TransactionTypeDto>(TransactionType) : null;
+        }
+
+        public Task<bool> Update(TransactionType Entity)
+        {
+            return _repository.Update(Entity);
+        }
+    }
+}
