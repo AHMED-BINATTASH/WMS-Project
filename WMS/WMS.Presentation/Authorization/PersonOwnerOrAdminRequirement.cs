871293a200acb230141c882b4284392405721ac8
nmtcode <nawaf92004@gmail.com>
2026-03-11 04:43:37 +0300

    handling somthing

--- WMS/WMS.Presentation/Authorization/PersonOwnerOrAdminRequirement.cs
+++ WMS/WMS.Presentation/Authorization/PersonOwnerOrAdminRequirement.cs
@@ -1,28 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using System.Security.Claims;
-public class PersonOwnerOrAdminHandler
-    : AuthorizationHandler<PersonOwnerOrAdminRequirement, int>
+
+// This class represents the authorization rule itself.
+// It does NOT contain logic.
+// It simply defines the requirement:
+// "Owner OR Admin can access the student resource."
+public class PersonOwnerOrAdminRequirement : IAuthorizationRequirement
 {
-    protected override Task HandleRequirementAsync(
-        AuthorizationHandlerContext context,
-        PersonOwnerOrAdminRequirement requirement,
-        int studentId)
-    {
-        if (context.User.IsInRole("Admin"))
-        {
-            // Mark the requirement as satisfied
-            context.Succeed(requirement);
-            return Task.CompletedTask;
-        }
-
-        var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-        if (int.TryParse(userId, out int authenticatedStudentId) &&
-            authenticatedStudentId == studentId)
-        {
-            context.Succeed(requirement);
-        }
-
-        return Task.CompletedTask;
-    }
 }
